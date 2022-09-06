38dc6f324193ef6d07ee440a0bb354737dee74df
Kevin Jacox <kev@jacox.org>
2022-09-06 17:53:24 -0600

    created models for billboard, artist and song

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_06_041838) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_06_235238) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "artists", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.integer "listeners"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "billboards", force: :cascade do |t|
+    t.string "title"
+    t.string "genre"
+    t.integer "year"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "songs", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.integer "listens"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
